568541804344259834f98ec2be1e872f4abc7dfc
deepakchauhan33 <deep651965@gmail.com>
2026-06-18 23:28:41 +0530

    Update

---
+++ src/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema(
+
+  {
+
+    name: {
+      type: String,
+      require: true
+    },
+
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+
+);
+
+const user = mongoose.model("User", userSchema);
+
+export default UserActivation;
