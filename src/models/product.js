00fe163425274c9d3fd9a75c05421aa8008cfe71
deepakchauhan33 <deep651965@gmail.com>
2026-06-21 01:52:41 +0530

    Define product schema

---
+++ src/models/product.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const { schema } = require('./user');
+
+const productSchema = new mongoose.Schema(
+
+  {
+    _id: {
+      type: Number,
+      required: true
+    },
+
+
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    price: {
+      type: String,
+      required: true,
+      min: 0
+    },
+
+    description: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    category: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    image: {
+      type: String,
+      required: true,
+    },
+
+    rating: {
+      rate: { type: Number, required: true, min: 0, max: 5 },
+      count: { type: Number, required: true, min: 0 }
+    }
+
+  },
+
+  {
+    timestamps: true;
+  }
+);
